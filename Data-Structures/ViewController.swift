16ac20de3b2f5a6f567fef937dc96ccd473b94ce
Akanksha Sharma <sharmaakanksha44@gmail.com>
2018-03-17 18:56:54 +0530

    - Bubble Sort Initial implementation

--- Data-Structures/ViewController.swift
+++ Data-Structures/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bucketSort()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +22,22 @@ class ViewController: UIViewController {
     }
 
 
+    func bucketSort(){
+        var arr = [12,9,7,5,3,1]
+
+        var temp = 0
+        print(arr)
+        for i in 0...(arr.count-2) {
+            for j in 0...(arr.count-i-2){
+               print(i,j)
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j+1]
+                    arr[j+1] = arr[j]
+                    arr[j] = temp
+                }
+            }
+        }
+        print(arr)
+    }
 }
 
